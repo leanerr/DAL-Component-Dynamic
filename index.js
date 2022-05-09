762a424de11daace6aebc03f713de55e06dbed1e
Ali Askari <real_asgari@yahoo.com>
2022-05-10 01:00:41 +0430

    index

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('<h1> Shiraz University Assignment 2 🤖🤖</h1>');
+});
+
+app.use('/api/employees', require('./api/employees'));
+app.use('/api/employees-variant', require('./api/employees-variant'));
+app.use('/api/users', require('./api/users'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started running on ${process.env.PORT} for ${process.env.NODE_ENV}`);
+});
